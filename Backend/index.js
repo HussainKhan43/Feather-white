0ffbe7a9b8c6a40dfe4a10ab6bf39d8c7838b4b9
HussainKhan43 <hussainkhan5march@gmail.com>
2025-03-02 15:55:43 +0530

    completed

--- Backend/index.js
+++ Backend/index.js
@@ -39,7 +39,6 @@ app.use(
 mongoose
   .connect(DB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
     serverSelectionTimeoutMS: 30000, // 30 seconds
     bufferCommands: false,
   })
